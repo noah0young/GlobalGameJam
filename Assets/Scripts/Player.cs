bad107be5651a2f21e91b60d3259752ca357250e
Noah <noah@Noahs-MacBook-Pro.local>
2022-01-29 14:28:44 -0500

    Spikes work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     private Animator myAnimator;
     [Header("General Physics")]
     private Rigidbody2D myRigidbody;
-    public bool onGround; // Is the player on the ground
+    private bool onGround; // Is the player on the ground
     public float upwardsGravity = .7f;
     // The scale of gravity when the player is jumping up
     public float downwardsGravity = 1f;
@@ -21,6 +21,13 @@ public class Player : MonoBehaviour
     // Length of the raycast to check if the player is on the ground.
     // This must be greater than half the height of the player, since
     // it starts in the center of the player object
+    [Header("General Movement")]
+    private bool canMove;
+    // Determines if the player can move
+    private bool facingRight;
+    public Vector2 bounce = new Vector2(8, 2);
+    // This is the speed of the bounce when you fly away from enemies or spikes
+    public float bounceTime = .2f;
     [Header("Human Movement")]
     public float humanMaxSpeed = 10f;
     public float humanAcc = .5f;
@@ -44,6 +51,8 @@ public class Player : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         curHealth = maxHealth;
         invisible = false;
+        facingRight = true;
+        canMove = true;
     }
 
     // Update is called once per frame
@@ -55,7 +64,7 @@ public class Player : MonoBehaviour
             AdjustGravity();
             GroundCheck();
             Jump();
-            changeFormStart();
+            //changeFormStart();
             Move(humanMaxSpeed, humanAcc, humanSlowDownAcc);
         }
         else if (playerState == PlayerState.Monster)
@@ -64,7 +73,7 @@ public class Player : MonoBehaviour
             AdjustGravity();
             GroundCheck();
             Punch();
-            changeFormStart();
+            //changeFormStart();
             Move(monstMaxSpeed, monstAcc, monstSlowDownAcc);
         }
         else if (playerState == PlayerState.Transforming)
@@ -97,47 +106,63 @@ public class Player : MonoBehaviour
     // This will move the player based on the direction held
     public void Move(float maxSpeed, float acc, float slowDown)
     {
-        float direction = Input.GetAxis("Horizontal");
-        Vector2 velocity = myRigidbody.velocity;
-        if (direction == 0)
+        if (canMove)
         {
-            //Slows down player
-            if (velocity.x > 0)
+            float direction = Input.GetAxis("Horizontal");
+            Vector2 velocity = myRigidbody.velocity;
+            if (direction == 0)
             {
-                velocity.x -= slowDown;
-                if (velocity.x < 0)
-                {
-                    velocity.x = 0;
-                }
-            }
-            else if (velocity.x < 0)
-            {
-                velocity.x += slowDown;
+                //Slows down player
                 if (velocity.x > 0)
                 {
-                    velocity.x = 0;
+                    velocity.x -= slowDown;
+                    if (velocity.x < 0)
+                    {
+                        velocity.x = 0;
+                    }
+                }
+                else if (velocity.x < 0)
+                {
+                    velocity.x += slowDown;
+                    if (velocity.x > 0)
+                    {
+                        velocity.x = 0;
+                    }
                 }
             }
-        }
-        else if (direction > 0)
-        {
-            // Increases velocity forwards
-            velocity.x += acc;
-            if (velocity.x > maxSpeed)
+            else if (direction > 0)
             {
-                velocity.x = maxSpeed;
+                // Increases velocity forwards
+                velocity.x += acc;
+                if (velocity.x > maxSpeed)
+                {
+                    velocity.x = maxSpeed;
+                }
             }
-        }
-        else if (direction < 0)
-        {
-            // Increases velocity backwards
-            velocity.x -= acc;
-            if (velocity.x < -maxSpeed)
+            else if (direction < 0)
             {
-                velocity.x = -maxSpeed;
+                // Increases velocity backwards
+                velocity.x -= acc;
+                if (velocity.x < -maxSpeed)
+                {
+                    velocity.x = -maxSpeed;
+                }
             }
+            myRigidbody.velocity = velocity;
+            SetDirectionFacing(velocity.x);
         }
-        myRigidbody.velocity = velocity;
+    }
+
+    private void SetDirectionFacing(float velocityX)
+    {
+        if (velocityX > 0)
+        {
+            facingRight = true;
+        }
+        else if (velocityX < 0)
+        {
+            facingRight = false;
+        }
     }
 
     // Updates the onGround variable based on if the player is on the ground or not
@@ -164,7 +189,7 @@ public class Player : MonoBehaviour
     }
     
     // This will change forms between the monster and the platformer
-    private void changeFormStart()
+    /*private void changeFormStart()
     {
         if (Input.GetKeyDown("k"))
         {
@@ -183,7 +208,7 @@ public class Player : MonoBehaviour
             playerState = PlayerState.Platformer;
         }
         return null;
-    }
+    }*/
 
     // Adds 1 health
     public void AddHealth()
@@ -240,5 +265,36 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(invisiblityTime);
         Debug.Log("Invisibility End");
         invisible = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Spikes")
+        {
+            LoseHealth();
+            BounceBack();
+        }
+    }
+
+    // This will launch you back when you take damage
+    private void BounceBack()
+    {
+        StartCoroutine(BounceFreeze());
+        if (facingRight)
+        {
+            myRigidbody.velocity = bounce * new Vector2(-1, 1);
+        }
+        else
+        {
+            myRigidbody.velocity = bounce;
+        }
+    }
+
+    // This will freeze player movement while they are bouncing back
+    private IEnumerator BounceFreeze()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(bounceTime);
+        canMove = true;
     }
 }
