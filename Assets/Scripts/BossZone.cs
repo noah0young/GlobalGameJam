40c7b960a6eb955dd1158ec15a962382238d3196
7max18 <maxkoppel0703@gmail.com>
2022-01-29 18:23:15 -0500

    Implemented boss barriers and lever

---
+++ Assets/Scripts/BossZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZone : MonoBehaviour
+{
+    public bool bossDefeated;
+    public GameObject lever;
+
+    private void Update()
+    {
+        if (bossDefeated)
+        {
+            lever.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                if(transform.GetChild(i).CompareTag("Pillar"))
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
