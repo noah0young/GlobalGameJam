db0386b153a612f62d41ace67eb1ed58afd00238
pierrefranklin <pierre.franklin@gmail.com>
2022-01-29 12:07:10 -0500

    Adds initial dialogue testing stuff

---
+++ Assets/Scripts/DialogueImporter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogueImporter : MonoBehaviour
+{
+    public TextMeshProUGUI displayText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (textFile != null)
+        {
+            textLines = textFile.text.Split('\n');
+        }
+    }
+
+    void Update()
+    {
+        if (currentLine < textLines.Length)
+        {
+            displayText.text = textLines[currentLine];
+        }
+        else
+        {
+            currentLine = 0;
+
+            int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.anyKeyDown)
+        {
+            currentLine += 1;
+        }
+    
+    }
+
+}
