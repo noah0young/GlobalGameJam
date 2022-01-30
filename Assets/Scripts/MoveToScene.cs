b6915c0196cce0dd748acf2fc23ec51855430aad
Noah <noah@Noahs-MacBook-Pro.local>
2022-01-30 18:06:43 -0500

    credits work

--- Assets/Scripts/MoveToScene.cs
+++ Assets/Scripts/MoveToScene.cs
@@ -7,9 +7,15 @@ public class MoveToScene : MonoBehaviour
 {
 
     public int sceneDestination;
+    //public string sceneDestinationStr;
     // Start is called before the first frame update
     public void changeScene()
     {
         SceneManager.LoadScene(sceneDestination);
+    }
+
+    public void changeSceneString(string sceneDestinationStr)
+    {
+        SceneManager.LoadScene(sceneDestinationStr);
     }
 }
