81a59c66539b535c8f15b19c417931c9aaecad80
Root_Astro <g.root.at.jam@gmail.com>
2022-01-29 19:39:29 -0800

    new file:   .vsconfig 	new file:   Assets/Enemy_Test.prefab 	new file:   Assets/Enemy_Test.prefab.meta 	new file:   Assets/Materials/Test_enemyMat.mat 	new file:   Assets/Materials/Test_enemyMat.mat.meta 	new file:   Assets/PreFab/Enemy_Test.prefab 	new file:   Assets/PreFab/Enemy_Test.prefab.meta 	new file:   Assets/PreFab/LevelGenerator.prefab 	new file:   Assets/PreFab/LevelGenerator.prefab.meta 	new file:   Assets/PreFab/TIle_Base_Test.prefab 	new file:   Assets/PreFab/TIle_Base_Test.prefab.meta 	new file:   Assets/PreFab/TIle_FloorBkgd_Test.prefab 	new file:   Assets/PreFab/TIle_FloorBkgd_Test.prefab.meta 	new file:   Assets/PreFab/TIle_Test.png 	new file:   Assets/PreFab/TIle_Test.png.meta 	new file:   Assets/PreFab/TIle_Top_Test.prefab 	new file:   Assets/PreFab/TIle_Top_Test.prefab.meta 	new file:   Assets/Scenes/Procedural_test.unity 	new file:   Assets/Scenes/Procedural_test.unity.meta 	modified:   Assets/Scenes/TestLevel.unity 	new file:   Assets/Scripts/CollisionSystem.meta 	new file:   Assets/Scripts/CollisionSystem/CollisionTrigger.cs 	new file:   Assets/Scripts/CollisionSystem/CollisionTrigger.cs.meta 	new file:   Assets/Scripts/CollisionSystem/ICollisionHAndler.cs 	new file:   Assets/Scripts/CollisionSystem/ICollisionHAndler.cs.meta 	new file:   Assets/Scripts/Enemy.cs 	new file:   Assets/Scripts/Enemy.cs.meta 	new file:   Assets/Scripts/LevelGenerator.cs 	new file:   Assets/Scripts/LevelGenerator.cs.meta 	new file:   Assets/Scripts/SpawnMG.cs 	new file:   Assets/Scripts/SpawnMG.cs.meta 	modified:   Packages/manifest.json 	new file:   ProjectSettings/Packages/com.xeleh.enhancer/Settings.asset 	modified:   ProjectSettings/TagManager.asset

---
+++ Assets/Scripts/CollisionSystem/CollisionTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    private ICollisionHAndler HAndler;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        HAndler = GetComponentInParent<ICollisionHAndler>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HAndler.CollisionEnter(gameObject.name, collision.gameObject);        
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ICollisionHAndler
+{
+    // Start is called before the first frame update
+    public float speed = 3.0f;
+    private Rigidbody enemRB;
+    //private GameObject PlayertoAtt;
+    private SpawnMG Manager;
+    //public int enem_Life = 1;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject weapon;
+    [SerializeField] private Transform GenTarget;
+    [SerializeField] private Transform MainTarget;
+
+    [SerializeField]private float AtckCD;
+    private bool AbleToAtck = true;
+    private float TimeSinceAtck;
+
+    //public ParticleSystem DeathParticle;
+    void Start()
+    {
+        enemRB = GetComponent<Rigidbody>();
+        //PlayertoAtt = GameObject.Find("Winston");
+        //enem_Life = 5;
+
+        //SPAWN MANAGER LINK!!!!!
+        //Manager = GameObject.Find("GameManager").GetComponent<SpawnMG>();
+        Debug.Log("Game Start");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3 lookdirection = (PlayertoAtt.transform.position - transform.position).normalized;
+        // enemRB.AddForce( lookdirection * speed);        
+        EnemAttack();
+    }
+    private void AtckEnd()
+    {
+        animator.SetBool("Attack", false);
+    }
+
+    private void EnemAttack()
+    {
+        //cooldown mechanic
+        if (!AbleToAtck)
+        {
+            TimeSinceAtck += Time.deltaTime;//cooldown in effect
+        }
+        if (TimeSinceAtck >= AtckCD)
+        {
+            AbleToAtck = true;//new attack
+        }
+        if (AbleToAtck && MainTarget!=null)
+        {
+            AbleToAtck = false;
+            TimeSinceAtck = 0;
+            animator.SetBool("Attack",true);
+        }
+    }
+    private void LookForPlayer()
+    {
+
+    }
+    public void CollisionEnter(string colliderName, GameObject other)
+    {
+        //enem_Life--;
+        //CREATE HIT FUNCTION FOR PLAYER
+
+        //needs fix transform on y
+        if (colliderName == "Sight" && other.tag == "Weapon")//follows
+        {
+            if(MainTarget == null)
+            {
+                this.MainTarget = other.transform;
+                Debug.Log("Looking at player and following");
+            }
+        }
+
+        if (other.tag == "Weapon" && Manager.MainAbility)//Enemy death condition
+        {
+
+            Destroy(this);
+            //DeathParticle.Play();
+            Debug.Log("Enemy dies!");
+        }
+
+        if (colliderName == "EnemyWeapon" && other.tag == "Player")//Enemy death condition
+        {
+
+            //TakingHit();
+            //ENEMYHitParticle.Play();
+            //Change the player bool by delta decrease - GetComponent<Player>().bool = true;
+            Debug.Log("Enemy jabs at the player!");
+        }
+
+    }
+    /*
+     TakingHit(){
+        if(player.Stats.LifeAmount > 0){//GameOver != true;
+            UIManager.Instance.LifeRemoved();//Or GameManager    
+            player.get
+        }
+    }
+     */
+
+
+    public void CollisionExit(string colliderName, GameObject other)
+    {
+        if (colliderName == "Sight" && other.tag =="Player")
+        {
+            MainTarget = null;
+        }
+    }
+}
