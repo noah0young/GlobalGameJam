0741d294633cb6bc406c2e3912a40d92852d4ed2
pierrefranklin <pierre.franklin@gmail.com>
2022-01-29 19:10:03 -0500

    Updates the script to handle the format of the script, adds the scripts for the first few scenes and adds the scenes

--- Assets/Scripts/MultiDialogueManager.cs
+++ Assets/Scripts/MultiDialogueManager.cs
@@ -39,11 +39,21 @@ public class MultiDialogueManager : MonoBehaviour
 
     void Update()
     {
-        if (currentLine < textLines.Length)
+
+        //This is an ugly way to iterate to the next valid line; I guess if we have time I can clean it up
+        while (currentLine < textLines.Length)
         {
 
             // Really these should only be set one time when the button is pressed, but I lazy
             string currentTextLine = textLines[currentLine];
+
+            // If this line begins with one of these characters, we don't want to read it (rtf formatting)
+            if (currentTextLine.Length == 0 || currentTextLine[0] == '{' || currentTextLine[0] == '}' || currentTextLine[0] == '\\')
+            {
+                currentLine += 1;
+                continue;
+            }
+            Debug.Log(currentTextLine);
 
             displayPanelHuman.SetActive(false);
             displayPanelDemon.SetActive(false);
@@ -55,31 +65,31 @@ public class MultiDialogueManager : MonoBehaviour
 
             switch (characterIndicator)
             {
-                case 'h':
+                case 'P':
                     displayPanelHuman.SetActive(true);
                     displayTextHuman.text = currentTextLine.Substring(2);
                     break;
-                case 'd':
+                case 'M':
                     displayPanelDemon.SetActive(true);
                     displayTextDemon.text = currentTextLine.Substring(2);
                     break;
-                case 'w':
+                case 'W':
                     displayPanelWitch.SetActive(true);
                     displayTextWitch.text = currentTextLine.Substring(2);
                     break;
                 default:
                     displayPanelHuman.SetActive(true);
-                    displayTextHuman.text = "Yo, check your indicators!";
+                    displayTextHuman.text = "Yo, check your indicators! START char is " + characterIndicator;
                     break;
             }
-
+            break;
         }
-        else
+        if (currentLine >=  textLines.Length)
         {
             currentLine = 0;
 
             int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(nextScene);
         }
 
         if (Input.anyKeyDown)
