34eec00491f91f7d8fdb480d1083b351c979fe74
pierrefranklin <pierre.franklin@gmail.com>
2022-01-30 16:40:44 -0500

    Adds tutorial mechanics

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+
+    public BoxCollider2D tutorialActivation;
+    public GameObject player;
+
+    public GameObject panel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D playerCollider = player.GetComponent<Collider2D>();
+        if (tutorialActivation.IsTouching(playerCollider))
+        {
+
+            panel.SetActive(true);
+        } else
+        {
+
+            panel.SetActive(false);
+
+        }
+    }
+}
