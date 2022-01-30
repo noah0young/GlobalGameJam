d8c51b7bb22073bd1c4bbabf0e97a04ade417f0a
Noah <noah@Noahs-MacBook-Pro.local>
2022-01-30 14:43:33 -0500

    Witch animations are incomplete and ambience music added

---
+++ Assets/Scripts/OnStartSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStartSound : MonoBehaviour
+{
+    private AudioSource audioSources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSources = GetComponent<AudioSource>();
+        audioSources.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
